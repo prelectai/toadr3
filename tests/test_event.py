eae1c85f3a60a2ff4b06697dd6ec8952bc2bc9e7
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2024-09-10 09:34:00 +0200

    Move create_event to separate file

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,53 +1,11 @@
 import datetime
 
 import pytest
+from testdata import create_event
 
 import toadr3
 
 UTC = datetime.timezone.utc
-
-
-def create_event() -> dict:
-    event = {
-        "id": "37",
-        "createdDateTime": "2024-08-15T08:52:55.578Z",
-        "modificationDateTime": "2024-08-15T08:53:41.127Z",
-        "objectType": "EVENT",
-        "programID": "69",
-        "eventName": "powerLimit",
-        "targets": [
-            {"type": "RESOURCE_NAME", "values": [1211]},
-            {"type": "ORGANIZATION_ID", "values": ["1337"]},
-        ],
-        "reportDescriptors": [
-            {
-                "payloadType": "POWER_LIMIT_ACKNOWLEDGEMENT",
-                "aggregate": False,
-                "startInterval": 0,
-                "numIntervals": -1,
-                "historical": False,
-                "frequency": -1,
-                "repeat": 1,
-                "targets": [],
-            }
-        ],
-        "payloadDescriptors": [
-            {
-                "payloadType": "CONSUMPTION_POWER_LIMIT",
-                "units": "KW",
-                "objectType": "EVENT_PAYLOAD_DESCRIPTOR",
-            }
-        ],
-        "intervalPeriod": {
-            "start": "2024-08-15T10:00:00.000Z",
-            "duration": "PT15M",
-            "randomizeStart": "PT0S",
-        },
-        "intervals": [
-            {"id": 0, "payloads": [{"type": "CONSUMPTION_POWER_LIMIT", "values": [1000]}]}
-        ],
-    }
-    return event
 
 
 def test_event():
