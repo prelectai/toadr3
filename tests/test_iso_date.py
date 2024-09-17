0ee860243b698efb59f21f2dc5db0fddf1ca9e27
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2024-09-17 13:09:18 +0200

    Add support for negative durations

--- tests/test_iso_date.py
+++ tests/test_iso_date.py
@@ -44,6 +44,22 @@ def test_duration_parse_errors():
         # no duration values
         parse_iso8601_duration("PT")
 
+    with pytest.raises(ValueError):
+        # missing duration unit
+        parse_iso8601_duration("P1")
+
+    with pytest.raises(ValueError):
+        # missing duration unit
+        parse_iso8601_duration("PT1")
+
+    with pytest.raises(ValueError):
+        # missing integer part
+        parse_iso8601_duration("PT.5S")
+
+    with pytest.raises(ValueError):
+        # missing fractional part
+        parse_iso8601_duration("PT5.S")
+
 
 def test_duration_parse():
     assert parse_iso8601_duration("P1W") == td(days=7)
@@ -63,3 +79,55 @@ def test_duration_parse():
     assert parse_iso8601_duration("PT1H1S") == td(hours=1, seconds=1)
     assert parse_iso8601_duration("PT1M1S") == td(minutes=1, seconds=1)
     assert parse_iso8601_duration("PT1.5S") == td(seconds=1, milliseconds=500)
+
+
+def test_duration_parse_negative():
+    assert parse_iso8601_duration("P-1D") == td(days=-1)
+    assert parse_iso8601_duration("-P1D") == td(days=-1)
+    assert parse_iso8601_duration("-P-1D") == td(days=1)
+
+    assert parse_iso8601_duration("P-1W") == td(days=-7)
+    assert parse_iso8601_duration("-P1W") == td(days=-7)
+    assert parse_iso8601_duration("-P-1W") == td(days=7)
+
+    assert parse_iso8601_duration("P-1W1D") == td(days=-6)
+    assert parse_iso8601_duration("-P-1W1D") == td(days=6)
+    assert parse_iso8601_duration("P1W-1D") == td(days=6)
+    assert parse_iso8601_duration("-P1W-1D") == td(days=-6)
+
+    assert parse_iso8601_duration("PT-1H") == td(hours=-1)
+    assert parse_iso8601_duration("-PT1H") == td(hours=-1)
+    assert parse_iso8601_duration("-PT-1H") == td(hours=1)
+
+    assert parse_iso8601_duration("PT-1M") == td(minutes=-1)
+    assert parse_iso8601_duration("-PT1M") == td(minutes=-1)
+    assert parse_iso8601_duration("-PT-1M") == td(minutes=1)
+
+    assert parse_iso8601_duration("PT-1S") == td(seconds=-1)
+    assert parse_iso8601_duration("-PT1S") == td(seconds=-1)
+    assert parse_iso8601_duration("-PT-1S") == td(seconds=1)
+
+    assert parse_iso8601_duration("PT-0.5S") == td(milliseconds=-500)
+    assert parse_iso8601_duration("-PT0.5S") == td(milliseconds=-500)
+    assert parse_iso8601_duration("-PT-0.5S") == td(milliseconds=500)
+
+    assert parse_iso8601_duration("PT-1M-1S") == td(minutes=-1, seconds=-1)
+    assert parse_iso8601_duration("PT1M-1S") == td(minutes=1, seconds=-1)
+    assert parse_iso8601_duration("PT-1M1S") == td(minutes=-1, seconds=1)
+    assert parse_iso8601_duration("-PT1M1S") == td(minutes=-1, seconds=-1)
+    assert parse_iso8601_duration("-PT-1M1S") == td(minutes=1, seconds=-1)
+    assert parse_iso8601_duration("-PT1M-1S") == td(minutes=-1, seconds=1)
+    assert parse_iso8601_duration("-PT-1M-1S") == td(minutes=1, seconds=1)
+
+    assert parse_iso8601_duration("PT-1M-0.5S") == td(minutes=-1, milliseconds=-500)
+    assert parse_iso8601_duration("PT1M-0.5S") == td(minutes=1, milliseconds=-500)
+    assert parse_iso8601_duration("PT-1M0.5S") == td(minutes=-1, milliseconds=500)
+    assert parse_iso8601_duration("-PT1M0.5S") == td(minutes=-1, milliseconds=-500)
+    assert parse_iso8601_duration("-PT-1M0.5S") == td(minutes=1, milliseconds=-500)
+    assert parse_iso8601_duration("-PT1M-0.5S") == td(minutes=-1, milliseconds=500)
+    assert parse_iso8601_duration("-PT-1M-0.5S") == td(minutes=1, milliseconds=500)
+
+    assert parse_iso8601_duration("P-1W-1DT-1H-1M-1S") == td(
+        days=-8, hours=-1, minutes=-1, seconds=-1
+    )
+    assert parse_iso8601_duration("-P-1W-1DT-1H-1M-1S") == td(days=8, hours=1, minutes=1, seconds=1)
