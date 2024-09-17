490b07858d81afa303e59d3afc8fec051aeaecc3
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2024-09-17 13:09:18 +0200

    Add support for negative durations

--- toadr3/iso_date.py
+++ toadr3/iso_date.py
@@ -13,20 +13,30 @@ def parse_iso8601_duration(duration: str) -> datetime.timedelta:
     Months and years are not supported because they have variable lengths and are not suitable for
     conversion to a fixed number of days without a reference date.
     """
+    dec_or_int = r"((\d+[.]\d+)|(\d+))"  # decimal or integer
     pattern = (
-        r"^P(?!$)"  # P is required to be at the start of the string
-        r"(?:(?P<weeks>\d+)W)?"  # weeks are optional
-        r"(?:(?P<days>\d+)D)?"  # days are optional
-        r"(?:T(?=\d)"  # T is required if time part is present
-        r"(?:(?P<hours>\d+)H)?"  # hours are optional
-        r"(?:(?P<minutes>\d+)M)?"  # minutes are optional
-        r"(?:(?P<seconds>\d+\.\d+|\d*)S)?"  # seconds are optional (can be fractional)
+        r"^(?:(?P<negative>[-]?))?"  # negative sign is optionally at the start of the string
+        r"P(?!$)"  # P is required to be at the start of the string
+        r"(?:(?P<weeks>[-]?\d+)W)?"  # weeks are optional
+        r"(?:(?P<days>[-]?\d+)D)?"  # days are optional
+        rf"(?:T(?=[-]?{dec_or_int}[HMS])"  # T is required if time part is present (+ look ahead)
+        r"(?:(?P<hours>[-]?\d+)H)?"  # hours are optional
+        r"(?:(?P<minutes>[-]?\d+)M)?"  # minutes are optional
+        rf"(?:(?P<seconds>[-]?{dec_or_int})S)?"  # seconds are optional (can be fractional)
         r")?$"  # end of string
     )
     match = re.match(pattern, duration)
     if not match:
         raise ValueError(f"Invalid ISO 8601 duration: {duration}")
-    parts = {k: float(v) for k, v in match.groupdict("0").items()}
+
+    factor = 1
+    parts = {}
+    for k, v in match.groupdict("0").items():
+        if k == "negative":
+            if v == "-":
+                factor = -1
+        else:
+            parts[k] = float(v) * factor
 
     if "weeks" in parts:
         if "days" not in parts:
