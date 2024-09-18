5b74230c600a550cd7a9a3d03250106d33236784
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2024-09-18 13:09:19 +0200

    Fix PyFlakes F401
    F401 <name> imported but unused; consider removing, adding to
    `__all__`, or using a redundant alias

--- toadr3/__init__.py
+++ toadr3/__init__.py
@@ -9,3 +9,21 @@ from .payload_descriptor import PayloadDescriptor
 from .report import Report, ReportData
 from .report_descriptor import ReportDescriptor
 from .values_map import parse_values_map
+
+__all__ = [
+    "AccessToken",
+    "acquire_access_token",
+    "Event",
+    "TargetType",
+    "get_events",
+    "SchemaException",
+    "ToadrException",
+    "Interval",
+    "IntervalPeriod",
+    "parse_iso8601_duration",
+    "PayloadDescriptor",
+    "Report",
+    "ReportData",
+    "ReportDescriptor",
+    "parse_values_map",
+]
