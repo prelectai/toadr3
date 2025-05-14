563242bd453f176f0f5a0e51f01071a188051d86
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2025-05-14 13:08:53 +0200

    Add OAuthConfig and acquire_access_token_from_config
    Introduced the `OAuthConfig` class to encapsulate authorization
    configuration and a new function `acquire_access_token_from_config` for
    streamlined token acquisition. Updated tests and `__all__` to include
    the new additions and incremented the library version to 0.14.1.

--- toadr3/__init__.py
+++ toadr3/__init__.py
@@ -1,13 +1,20 @@
 from . import models
-from .access_token import AccessToken, acquire_access_token
+from .access_token import (
+    AccessToken,
+    OAuthConfig,
+    acquire_access_token,
+    acquire_access_token_from_config,
+)
 from .events import get_events
 from .exceptions import ToadrError
 from .reports import get_reports, post_report
 
 __all__ = [
     "AccessToken",
+    "OAuthConfig",
     "ToadrError",
     "acquire_access_token",
+    "acquire_access_token_from_config",
     "get_events",
     "get_reports",
     "models",
