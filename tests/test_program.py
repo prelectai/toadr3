c7d4a50ef137776b0926fce4440f867dd9bcc738
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2025-10-17 14:06:42 +0200

    Add Program model for program-specific metadata

---
+++ tests/test_program.py
@@ -0,0 +1,48 @@
+import datetime
+
+import toadr3
+
+UTC = datetime.timezone.utc
+
+
+def test_program_defaults() -> None:
+    program = toadr3.models.Program(
+        program_name="Test Program",
+    )
+
+    assert program.id is None
+    assert program.created_date_time is None
+    assert program.modification_date_time is None
+    assert program.object_type == "EVENT"
+    assert program.program_name == "Test Program"
+    assert program.program_long_name is None
+    assert program.retailer_name is None
+    assert program.retailer_long_name is None
+    assert program.program_type is None
+    assert program.country is None
+    assert program.principal_subdivision is None
+    assert program.time_zone_offset == datetime.timedelta(seconds=0)
+    assert program.interval_period is None
+    assert program.program_descriptions is None
+    assert program.binding_events is None
+    assert program.local_price is None
+    assert program.payload_descriptors is None
+    assert program.targets is None
+
+
+def test_program_sample() -> None:
+    sample_1 = """{
+        "id": "0",
+        "createdDateTime": "2025-08-21T07:13:00Z",
+        "modificationDateTime": "2025-08-23T08:10:00Z",
+        "programName": "HB",
+        "programLongName": "Heartbeat"
+    }"""
+
+    program = toadr3.models.Program.model_validate_json(sample_1)
+
+    assert program.id == "0"
+    assert program.program_name == "HB"
+    assert program.program_long_name == "Heartbeat"
+    assert program.created_date_time == datetime.datetime(2025, 8, 21, 7, 13, 0, tzinfo=UTC)
+    assert program.modification_date_time == datetime.datetime(2025, 8, 23, 8, 10, 0, tzinfo=UTC)
---
+++ toadr3/models/program.py
@@ -0,0 +1,68 @@
+import datetime
+from typing import Literal
+
+from pydantic import AnyUrl, Field
+
+from .docstringbasemodel import DocstringBaseModel
+from .eventpayloaddescriptor import EventPayloadDescriptor
+from .intervalperiod import IntervalPeriod
+from .reportpayloaddescriptor import ReportPayloadDescriptor
+from .valuesmap import ValuesMap
+
+
+class Program(DocstringBaseModel):
+    """Provides program specific metadata from VTN to VEN."""
+
+    id: str | None = Field(min_length=1, max_length=128, pattern="^[a-zA-Z0-9_-]*$", default=None)
+    """VTN provisioned on object creation."""
+
+    created_date_time: datetime.datetime | None = None
+    """VTN provisioned on object creation."""
+
+    modification_date_time: datetime.datetime | None = None
+    """VTN provisioned on object modification."""
+
+    object_type: Literal["EVENT"] = "EVENT"
+    """VTN provisioned on object creation."""
+
+    program_name: str = Field(min_length=1, max_length=128)
+    """Short name to uniquely identify program."""
+
+    program_long_name: str | None = None
+    """Long name of program for human readability."""
+
+    retailer_name: str | None = None
+    """Short name of energy retailer providing the program."""
+
+    retailer_long_name: str | None = None
+    """Long name of energy retailer for human readability."""
+
+    program_type: str | None = None
+    """A program defined categorization."""
+
+    country: str | None = None
+    """Alpha-2 code per ISO 3166-1."""
+
+    principal_subdivision: str | None = None
+    """Coding per ISO 3166-2. E.g. state in US."""
+
+    time_zone_offset: datetime.timedelta = datetime.timedelta(seconds=0)
+    """Number of hours different from UTC for the standard time applicable to the program."""
+
+    interval_period: IntervalPeriod | None = None
+    """The temporal span of the program, could be years long."""
+
+    program_descriptions: list[AnyUrl] | None = None
+    """A list of program descriptions."""
+
+    binding_events: bool | None = None
+    """True if events are fixed once transmitted."""
+
+    local_price: bool | None = None
+    """True if events have been adapted from a grid event."""
+
+    payload_descriptors: list[EventPayloadDescriptor | ReportPayloadDescriptor] | None = None
+    """A list of payloadDescriptors."""
+
+    targets: list[ValuesMap] | None = None
+    """A list of valuesMap objects."""
