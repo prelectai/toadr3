7c7fd755aea48a4ea55fb04ec602201c24e214e5
Jean-Paul Balabanian <jean-paul.balabanian@eviny.no>
2025-10-29 13:59:06 +0100

    fix JSON encoding of unset object_type defaults

--- tests/test_json_encoder.py
+++ tests/test_json_encoder.py
@@ -1,8 +1,11 @@
 import datetime
+from typing import Any
 
+import pytest
+from pydantic import BaseModel
 from testdata import create_event, create_report
 
-import toadr3
+from toadr3.models import Event, Program, Report, Subscription
 
 from_iso = datetime.datetime.fromisoformat
 
@@ -10,8 +13,8 @@ from_iso = datetime.datetime.fromisoformat
 def test_json_encoder_report() -> None:
     data = create_report()
 
-    report = toadr3.models.Report.model_validate(data)
-    result = toadr3.models.Report.model_dump(report, exclude_none=True, exclude_unset=True)
+    report = Report.model_validate(data)
+    result = Report.model_dump(report, exclude_none=True, exclude_unset=True)
 
     # for comparison, we expect datetime objects and not strings
     data["createdDateTime"] = from_iso(data["createdDateTime"])
@@ -27,8 +30,8 @@ def test_json_encoder_report() -> None:
 def test_json_encoder_event() -> None:
     data = create_event()
 
-    event = toadr3.models.Event.model_validate(data)
-    result = toadr3.models.Event.model_dump(event, exclude_none=True, exclude_unset=True)
+    event = Event.model_validate(data)
+    result = Event.model_dump(event, exclude_none=True, exclude_unset=True)
 
     # for comparison, we expect datetime objects and not strings
     data["createdDateTime"] = from_iso(data["createdDateTime"])
@@ -38,3 +41,77 @@ def test_json_encoder_event() -> None:
     data["intervalPeriod"]["start"] = from_iso(data["intervalPeriod"]["start"])
 
     assert result == data
+
+
+def default_event() -> dict[str, Any]:
+    return {
+        "programID": "11",
+        "intervals": [
+            {
+                "id": 1,
+                "payloads": [{"type": "type", "values": ["value1"]}],
+            }
+        ],
+    }
+
+
+def default_report() -> dict[str, Any]:
+    return {
+        "programID": "11",
+        "eventID": "33",
+        "clientName": "YAC",
+        "resources": [
+            {
+                "resourceName": "resource1",
+                "intervals": [
+                    {
+                        "id": 1,
+                        "payloads": [{"type": "type", "values": ["value1"]}],
+                    }
+                ],
+            }
+        ],
+    }
+
+
+def default_program() -> dict[str, Any]:
+    return {
+        "programName": "pname",
+    }
+
+
+def default_subscription() -> dict[str, Any]:
+    return {
+        "clientName": "YAC",
+        "programID": "11",
+        "objectOperations": [
+            {
+                "objects": ["EVENT"],
+                "operations": ["POST"],
+                "callbackUrl": "https://example.com/callback",
+            }
+        ],
+    }
+
+
+@pytest.mark.parametrize(
+    ("model_class", "model_data"),
+    [
+        (Event, default_event()),
+        (Report, default_report()),
+        (Program, default_program()),
+        (Subscription, default_subscription()),
+    ],
+)
+def test_json_encode_as_expected(model_class: type[BaseModel], model_data: dict[str, Any]) -> None:
+    # 1. we expect only default values to be part of the final JSON
+    # 2. we expect the 'objectType' field to be included in the final JSON
+    instance = model_class.model_validate(model_data)
+    result = model_class.model_dump(instance, exclude_none=True, exclude_unset=True)
+
+    assert "objectType" not in model_data
+    assert "objectType" in result
+    assert result["objectType"] == model_class.__name__.upper()
+
+    model_data["objectType"] = model_class.__name__.upper()
+    assert result == model_data
